8764653196381f12e94f5221ce7cf9a21ab2da9a
Victorien <victorien.fischer@epitech.eu>
2017-09-25 08:40:24 -0700

    started server (+ added some files to gitignore)

---
+++ server/headers/Server.hh
@@ -0,0 +1,32 @@
+//
+// Server.hh for cpp_babel in /home/vicky/Bureau/EPITECH/cpp_babel/server/headers
+// 
+// Made by Victorien Fischer
+// Login   <victorien.fischer@epitech.eu>
+// 
+// Started on  Mon Sep 25 06:40:28 2017 Victorien Fischer
+// Last update Mon Sep 25 08:35:50 2017 Victorien Fischer
+//
+
+#ifndef __BABEL_SERVER__
+# define __BABEL_SERVER__
+
+class Server
+{
+private:
+  unsigned short	_port;
+  char			_running;
+
+public:
+  Server();
+  ~Server();
+
+public:
+  unsigned short	getPort() const;
+  void			setPort(unsigned short);
+
+public:
+  int	run();
+};
+
+#endif /* !__BABEL_SERVER__ */
