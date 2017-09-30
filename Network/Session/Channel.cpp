3d8f68bc07af9416b312f30a21644fe3224bfd96
Arnaud WURMEL <arnaud.wurmel@epitech.eu>
2017-09-30 18:51:09 +0200

    channel cpp

---
+++ Network/Session/Channel.cpp
@@ -0,0 +1,49 @@
+//
+// Created by wurmel_a on 30/09/17.
+//
+
+#include "Channel.h"
+
+babel::Channel::Channel(std::string const& name) {
+    _name = name;
+}
+
+std::string const& babel::Channel::getName() const {
+    return _name;
+}
+
+std::set<std::shared_ptr<babel::User> > const&  babel::Channel::getUserList() const {
+    return _memberList;
+}
+
+bool    babel::Channel::join(std::shared_ptr<babel::User> const& user) {
+    user->leaveChannel();
+    _memberList.insert(user);
+    return true;
+}
+
+bool    babel::Channel::leave(std::shared_ptr<babel::User> const& user) {
+    std::set<std::shared_ptr<babel::User> >::const_iterator it = _memberList.begin();
+
+    while (it != _memberList.end()) {
+        if ((*it)->getId() == user->getId()) {
+            _memberList.erase(it);
+            return true;
+        }
+        ++it;
+    }
+    return false;
+}
+
+void    babel::Channel::triggerChannelEvent(User const &caller, std::string const &body) {
+    std::set<std::shared_ptr<babel::User> >::const_iterator it = _memberList.begin();
+
+    while (it != _memberList.end()) {
+        if ((*it)->getId() != caller.getId()) {
+            (*it)->sendResponse(Message::MessageType::Event, body);
+        }
+        ++it;
+    }
+}
+
+babel::Channel::~Channel() {}
