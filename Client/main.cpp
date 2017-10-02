f1ac9d485b0aa2f2771bc6df367895aee4cb8500
Ludovic Gueth <ludovicgueth@gmail.com>
2017-10-02 11:10:08 +0200

    test on main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -2,6 +2,8 @@
 #include <QApplication>
 #include <QMainWindow>
 #include "Socket/QtSocket.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
 int main(int ac, char **av) {
     QApplication    app(ac, av);
@@ -14,5 +16,19 @@ int main(int ac, char **av) {
     socket.startSession();
     std::cout << socket.connectSocket("127.0.0.1", 8080) << std::endl;
     socket.write(message);
+
+
+    //SCENE
+    QGraphicsScene scene;
+
+    //VIEW
+    QGraphicsView view(&scene);
+    view.setMinimumHeight(150);
+    view.setMinimumWidth(150);
+    view.setWindowTitle("cpp_babel");
+
+
+
+    view.show();
     return app.exec();
 }
