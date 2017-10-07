65fc917f35b81d07d71f46fb1ebf58273e59bd5f
Victorien <victorien.fischer@epitech.eu>
2017-10-07 02:03:24 +0200

    fixed sound on windows

--- Client/Audio/Record.cpp
+++ Client/Audio/Record.cpp
@@ -1,10 +1,13 @@
 #include "Record.hpp"
 
 Record::Record() {
+	this->stream = NULL;
   if ((this->err = Pa_Initialize()) != paNoError)
     std::cout << "Error : " << Pa_GetErrorText(err) << '\n';
   this->state = PA_OFF;
   this->inputParameters.device = Pa_GetDefaultInputDevice();
+  if (this->inputParameters.device == paNoDevice)
+	  throw (std::exception("yoyo"));
   this->inputParameters.channelCount = 2;
   this->inputParameters.sampleFormat = paFloat32;
   this->inputParameters.suggestedLatency =
@@ -59,11 +62,11 @@ DecodedFrame Record::RecordedFrames() {
 
 bool Record::startAudio() {
 	if (Pa_OpenStream(&this->stream, &this->inputParameters, NULL, SAMPLE_RATE,
-		FRAMES_PER_BUFFER, paClipOff, Pa_callBack, this) != paNoError) {
-		Pa_StartStream(this->stream);
+		FRAMES_PER_BUFFER, paClipOff, Pa_callBack, this) == paNoError && Pa_StartStream(this->stream) == paNoError) {
 		this->state = PA_ON;
 		return (true);
 	}
+	this->stream = NULL;
 	return (false);
 }
 bool Record::stopAudio() {
